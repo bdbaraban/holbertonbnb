1f990adc05c8e59f79783478f87fe744449e04a6
bdbaraban <375@holbertonschool.com>
2019-05-17 23:11:39 +0000

    Update jQuery script to check the status of the API and add available class accordingly

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,19 @@
+$('document').ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+});
