629e936bb4a718112575f9ee52062ffb04ad9d7d
bdbaraban <375@holbertonschool.com>
2019-05-18 21:54:07 +0000

    Implement dynamic checkboxes on locations filter

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,38 @@
+$('document').ready(function () {
+  const api = 'http://' + window.location.hostname;
+  $.get(api + ':5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  let locations = {};
+  $('.locations INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      locations[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete locations[$(this).attr('data-id')];
+    }
+    if (Object.values(locations).length === 0) {
+      $('.locations H4').html('&nbsp;');
+    } else {
+      $('.locations H4').text(Object.values(locations).join(', '));
+    }
+  });
+
+  let amenities = {};
+  $('.amenities INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
+  });
+});
