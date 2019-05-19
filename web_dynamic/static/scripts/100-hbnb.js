104f4be0e6073f01a96e7fdec9d7ea9b76981195
bdbaraban <375@holbertonschool.com>
2019-05-19 01:14:48 +0000

    Implement separate states and cities filtering logic

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,6 +1,7 @@
 $('document').ready(function () {
   const api = 'http://' + window.location.hostname;
-  $.get(api + ':5001/api/v1/status/', function (response) {
+
+  $.get(api + ':5001:/api/v1/status/', function (response) {
     if (response.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
@@ -8,13 +9,38 @@ $('document').ready(function () {
     }
   });
 
-  let locations = {};
-  $('.locations INPUT[type="checkbox"]').change(function () {
+  $.ajax({
+    url: api + ':5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: appendPlaces
+  });
+
+  let states = {};
+  $('.locations > UL > H2 > INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
-      locations[$(this).attr('data-id')] = $(this).attr('data-name');
+      states[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete locations[$(this).attr('data-id')];
+      delete states[$(this).attr('data-id')];
     }
+    const locations = Object.assign({}, states, cities);
+    if (Object.values(locations).length === 0) {
+      $('.locations H4').html('&nbsp;');
+    } else {
+      $('.locations H4').text(Object.values(locations).join(', '));
+    }
+  });
+
+  let cities = {};
+  $('.locations > UL > UL > LI INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      cities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete cities[$(this).attr('data-id')];
+    }
+    const locations = Object.assign({}, states, cities);
     if (Object.values(locations).length === 0) {
       $('.locations H4').html('&nbsp;');
     } else {
@@ -35,4 +61,53 @@ $('document').ready(function () {
       $('.amenities H4').text(Object.values(amenities).join(', '));
     }
   });
+
+  $('BUTTON').click(function () {
+    $.ajax({
+      url: api + ':5001/api/v1/places_search/',
+      type: 'POST',
+      data: JSON.stringify({
+        'states': Object.keys(states),
+        'cities': Object.keys(cities),
+        'amenities': Object.keys(amenities)
+      }),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: appendPlaces
+    });
+  });
 });
+
+function appendPlaces (data) {
+  $('SECTION.places').empty();
+  $('SECTION.places').append(data.map(place => {
+    return `<ARTICLE>
+              <DIV class="title">
+                <H2>${place.name}</H2>
+                  <DIV class="price_by_night">
+                    ${place.price_by_night}
+                  </DIV>
+                </DIV>
+                <DIV class="information">
+                  <DIV class="max_guest">
+                    <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.max_guest} Guests
+                  </DIV>
+                  <DIV class="number_rooms">
+                    <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.number_rooms} Bedrooms
+                  </DIV>
+                  <DIV class="number_bathrooms">
+                    <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.number_bathrooms} Bathrooms
+                  </DIV>
+                </DIV>
+                <DIV class="description">
+                  ${place.description}
+                </DIV>
+              </ARTICLE>`;
+  }));
+}
