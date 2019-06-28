ff86192486439957180982658868b782c2fe40f1
bdbaraban <375@holbertonschool.com>
2019-06-28 11:58:30 -0700

    Implement fetching and displaying of users in place articles

--- web_flask/static/scripts/dynamic.js
+++ web_flask/static/scripts/dynamic.js
@@ -85,36 +85,45 @@ $('document').ready(function() {
 // Dynamically insert places
 function appendPlaces(data) {
   $('SECTION.places').empty();
-  $('SECTION.places').append(
-    data.map(place => {
-      return `<ARTICLE>
-              <DIV class="title">
-                <H2>${place.name}</H2>
-                  <DIV class="price_by_night">
-                    ${place.price_by_night}
-                  </DIV>
-                </DIV>
-                <DIV class="information">
-                  <DIV class="max_guest">
-                    <I class="fa fa-users fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.max_guest} Guests
-                  </DIV>
-                  <DIV class="number_rooms">
-                    <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.number_rooms} Bedrooms
-                  </DIV>
-                  <DIV class="number_bathrooms">
-                    <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.number_bathrooms} Bathrooms
-                  </DIV>
-                </DIV>
-                <DIV class="description">
-                  ${place.description}
-                </DIV>
-              </ARTICLE>`;
-    })
-  );
+  $('SECTION.places').append('<H1>Places</H1>');
+  for (let place of data) {
+    $.get('https://bdbnb.site/api/v1/users/' + place.user_id, function(
+      response
+    ) {
+      user = response;
+      $('SECTION.places').append(
+        `<ARTICLE>
+          <DIV class="name_and_price">
+            <H2>${place.name}</H2>
+            <DIV class="price_by_night">
+              ${place.price_by_night}
+            </DIV>
+          </DIV>
+          <DIV class="information">
+            <DIV class="max_guest">
+              <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+              </BR>
+              ${place.max_guest} Guests
+            </DIV>
+            <DIV class="number_rooms">
+              <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+              </BR>
+              ${place.number_rooms} Bedrooms
+            </DIV>
+            <DIV class="number_bathrooms">
+              <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+              </BR>
+              ${place.number_bathrooms} Bathrooms
+            </DIV>
+          </DIV>
+          <DIV class="user"><STRONG>Owner: </STRONG>${user.first_name} ${
+          user.last_name
+        }</div>
+          <DIV class="description">
+            ${place.description}
+          </DIV>
+        </ARTICLE>`
+      );
+    });
+  }
 }
