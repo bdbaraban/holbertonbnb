0244714c32d25705b33bb7bdc5144976d13fbf0e
bdbaraban <375@holbertonschool.com>
2019-06-28 00:13:43 +0000

    Open CORS policy on /api/v1 route and externally load Swagger static content

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,12 +8,13 @@ from flasgger import Swagger, swag_from
 from models import storage
 from api.v1.views import app_views
 
-
+# Flask configuration
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+# Flasgger configuration
 template = {
     "swagger": "2.0",
     "info": {
@@ -21,6 +22,12 @@ template = {
         "description": "RESTful API for HolbertonBnB",
     }
 }
+# Externally load Swagger static content
+swagger_config = Swagger.DEFAULT_CONFIG
+swagger_config['swagger_ui_bundle_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js'
+swagger_config['swagger_ui_standalone_preset_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js'
+swagger_config['jquery_js'] = '//unpkg.com/jquery@2.2.4/dist/jquery.min.js'
+swagger_config['swagger_ui_css'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui.css'
 swagger = Swagger(app, template=template)
 
 
